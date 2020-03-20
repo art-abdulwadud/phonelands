55964fb1055f1b63c46ddb37d4abd36b0eee45dd
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-03-20 22:12:14 +0300

    js dropdown toggle

--- toggle.js
+++ toggle.js
@@ -1,11 +1,12 @@
 // DOM Elements
 const menuToggle = document.querySelector('.toggler');
-const smallNav = document.querySelector('.small-nav');
 const nav = document.querySelector('#nav');
+const droplink = document.querySelector('.droplink');
+const navDropdown = document.querySelector('#nav-dropdown-wrapper');
 const less = document.querySelector('.less');
 const more = document.querySelector('.more');
 
-// Toggle navbar for smaller devices
+// Toggle navbar on smaller devices
 let smallNavActive = false;
 
 menuToggle.addEventListener('click', () => {
@@ -15,5 +16,17 @@ menuToggle.addEventListener('click', () => {
 	} else {
 		nav.setAttribute('class', 'small-nav');
 		smallNavActive = true;
+	}
+});
+
+// Toggle the dropdown menu smaller devices
+let dropdownActive = false;
+droplink.addEventListener('click', () => {
+	if (dropdownActive) {
+		navDropdown.setAttribute('class', 'nav-dropdown');
+		dropdownActive = false;
+	} else {
+		navDropdown.setAttribute('class', 'nav-dropdown small-nav-dropdown');
+		dropdownActive = true;
 	}
 });
